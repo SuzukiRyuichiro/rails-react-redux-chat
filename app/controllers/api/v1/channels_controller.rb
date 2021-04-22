a0f50569910bdf9c4f66d633816f3be40a5790c2
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-22 12:30:42 +0900

    cped old API

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::ChannelsController < ApplicationController
+  def index
+    @channels = Channel.all
+    render json: {channels: @channels}
+  end
+
+  def create
+    @channel = Channel.new(name: params[:name])
+    if @channel.save
+      render json: { status: 'SUCCESS', channel: @channel}
+    else
+      render json: { status: 'ERROR', errors: @channel.errors }
+    end
+  end
 end
--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::MessagesController < ApplicationController
+  def index
+    @channel = Channel.find_by(name: params[:channel_id])
+    if @channel
+      @messages = @channel.messages
+      render json: { channel: @channel, messages: @messages}
+    else
+      render json: { channel: "not found", messages: []}
+    end
+  end
+
+  def create
+    channel = Channel.find_by(name: params[:channel_id])
+    if channel
+      message = set_message(channel)
+      save_message(message)
+    else
+      new_channel = Channel.create(name: params[:channel_id])
+      message = set_message(new_channel)
+      save_message(message)
+    end
+  end
+
+  private
+
+  def set_message(channel)
+    # expects a instance of a Channel
+    Message.new(content: params[:content], author: params[:author], channel: channel)
+  end
+
+  def save_message(message)
+    # expects an instance of a messge
+    if message.save
+      render json: { status: 'SUCCESS', message: message }
+    else
+      render json: { status: 'ERROR', errors: message.errors }
+    end
+  end
 end

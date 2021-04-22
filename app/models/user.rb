2e57f67133e28ee6308af09d1a3c99a95fd36310
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-22 17:47:39 +0900

    authentication token will now be generated for every user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :token_authenticatable
 
   validates :username, presence: true
   validates :username, uniqueness: true
+
+  before_save :ensure_authentication_token
 end
